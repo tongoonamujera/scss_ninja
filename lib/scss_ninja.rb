d2878ca60d56cc333bedf2d7e9f855347e302533
tongoonamujera <tongoonamujera@gmail.com>
2021-10-30 08:23:13 +0200

    Initial commit

---
+++ lib/scss_ninja.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "scss_ninja/version"
+
+module ScssNinja
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/scss_ninja_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ScssNinja do
+  it "has a version number" do
+    expect(ScssNinja::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
