06656e643448274ee7f205600ab440eaa55f506c
tongoonamujera <tongoonamujera@gmail.com>
2021-11-10 10:00:38 +0200

    Add rails engine

--- lib/scss_ninja.rb
+++ lib/scss_ninja.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "scss_ninja/version"
-require_relative "scss_ninja/scss_ninja_engine.rb"
+require_relative "scss_ninja/scss_ninja_engine"
 
 module ScssNinja
   class Error < StandardError; end
   # Your code goes here...
+  class Engine < ::Rails::Engine
+  end
 end
